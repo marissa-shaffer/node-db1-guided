38d503f71d2724fe670fa2c74eebeb12a50fde14
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-10-13 20:36:10 -0500

    finished in class

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,24 +3,76 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
-
+router.get("/", async (req, res, next) => {
+    try {
+        const messages = await db.select("*").from("messages")
+        res.json(messages)
+    } catch(err) {
+        next(err)
+    }
 })
 
-router.get("/:id", (req, res, next) => {
-
+router.get("/:id", async (req, res, next) => {
+    try {
+        const message = await db.select("*").from("messages").where("id", req.params.id).limit(1)
+        res.json(messages)
+    } catch(err) {
+        next(err)
+    }
 })
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+    try {
+       const payload = {
+           title: req.body.title,
+           contents: req.body.contents,
+       }
 
+       if (!payload.title || !payload.contents) {
+           return res.status(400).json({
+               message: "Need a title and contents",
+           })
+       }
+       const [id] = await db.insert(payload).into("messages")
+       const message = await db.insert(payload).into("messages")
+
+       res.status(201).json(messages)
+    } catch(err) {
+        next(err)
+    }
 })
 
-router.put("/:id", (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
+    try {
+        const payload = {
+            title: req.body.title,
+            contents: req.body.contents,
+        }
+ 
+        if (!payload.title || !payload.contents) {
+            return res.status(400).json({
+                message: "Need a title and contents",
+            })
+        }
+        await db("messages").where("id", req.params.id).update(payload)
 
+        const message = await db.first("*").from("messages").where("id", req.params.id)
+        res.json(message)
+
+     } catch(err) {
+         next(err)
+     }
 })
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
+ try {
+        await db("messages").where("id", req.params.id).del()
 
+        res.status(204).end()
+        
+    } catch(err) {
+        next(err)
+    }
 })
 
 module.exports = router
